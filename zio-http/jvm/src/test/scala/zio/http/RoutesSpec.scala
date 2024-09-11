5dde44df68df503490d21ab362e7ccf5bfde07d6
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-09-11 18:35:00 +0530

    Update RoutesSpec.scala

--- zio-http/jvm/src/test/scala/zio/http/RoutesSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/RoutesSpec.scala
@@ -24,15 +24,6 @@ import zio.http.codec.PathCodec
 object RoutesSpec extends ZIOHttpSpec {
   def extractStatus(response: Response): Status = response.status
 
-  // val authContext: HandlerAspect[Any, String] = HandlerAspect.customAuthProviding[String] { request =>
-  //   request.headers.get(Header.Authorization).flatMap {
-  //     case Header.Authorization.Basic(uname, secret) if uname.reverse == secret.value.mkString =>
-  //       Some(uname)
-  //     case _                                                                                   =>
-  //       None
-  //   }
-  // }
-
   def spec = suite("RoutesSpec")(
     test("empty not found") {
       val app = Routes.empty
@@ -124,13 +115,19 @@ object RoutesSpec extends ZIOHttpSpec {
         long <- ZIO.service[Long]
       } yield s"Int: $int, Long: $long"
 
-      val loggingAspect: HandlerAspect[Any, Nothing]               = HandlerAspect.before { _ =>
-        ZIO.log("Request received")
-      }
       val handler: Handler[Int & Long, Nothing, Request, Response] =
         Handler.fromZIO(routeWithMultipleDeps.map(result => Response.text(result)))
 
-      val route: Route[Int & Long, Nothing] = RoutePattern(Method.GET, Path.root) -> handler @@ loggingAspect
+      val authContext: HandlerAspect[Any, String] = HandlerAspect.customAuthProviding[String] { request =>
+        request.headers.get(Header.Authorization).flatMap {
+          case Header.Authorization.Basic(uname, secret) if uname.reverse == secret.value.mkString =>
+            Some(uname)
+          case _                                                                                   =>
+            None
+        }
+      }
+
+      val route: Route[Int & Long, Nothing] = RoutePattern(Method.GET, Path.root) -> (handler @@ authContext)
 
       val routes       = Routes(route)
       val env          = ZEnvironment(42).add(100L)
