f4351bc0d875051e850dddea060add5b124d4990
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-09-19 18:21:58 +0530

    add tests 2

--- zio-http/js/src/test/scala/zio/http/PathCodecPlatformSpecificSpec.scala
+++ zio-http/js/src/test/scala/zio/http/PathCodecPlatformSpecificSpec.scala
@@ -1,32 +1,34 @@
-package zio.http
+package zio.http.codec
 
 import zio.test._
 import zio.test.Assertion._
 
-object PathCodecJSPlatformSpecificSpec extends ZIOSpecDefault {
+object PathCodecPlatformSpecificSpec extends ZIOSpecDefault {
 
-  def spec = suite("PathCodecPlatformSpecificSpec")(
+  def spec = suite("PathCodecJSPlatformSpecificSpec")(
     test("parseInt should correctly parse a valid integer from a CharSequence") {
       val charSequence = "12345"
-      val result       = new PathCodecJSPlatformSpecific {}.parseInt(charSequence, 0, charSequence.length, 10)
+      val result       = new PathCodecPlatformSpecific {}.parseInt(charSequence, 0, charSequence.length, 10)
       assert(result)(equalTo(12345))
     },
     test("parseInt should throw an error for an invalid radix") {
       val charSequence = "12345"
-      assertThrows[NumberFormatException] {
-        new PathCodecJSPlatformSpecific {}.parseInt(charSequence, 0, charSequence.length, Character.MAX_RADIX + 1)
+      val exception    = intercept[NumberFormatException] {
+        new PathCodecPlatformSpecific {}.parseInt(charSequence, 0, charSequence.length, Character.MAX_RADIX + 1)
       }
+      assert(exception.getMessage)(containsString("radix"))
     },
     test("parseLong should correctly parse a valid long from a CharSequence") {
       val charSequence = "123456789012345"
-      val result       = new PathCodecJSPlatformSpecific {}.parseLong(charSequence, 0, charSequence.length, 10)
+      val result       = new PathCodecPlatformSpecific {}.parseLong(charSequence, 0, charSequence.length, 10)
       assert(result)(equalTo(123456789012345L))
     },
     test("parseLong should throw an error for an invalid input") {
       val charSequence = "invalid123"
-      assertThrows[NumberFormatException] {
-        new PathCodecJSPlatformSpecific {}.parseLong(charSequence, 0, charSequence.length, 10)
+      val exception    = intercept[NumberFormatException] {
+        new PathCodecPlatformSpecific {}.parseLong(charSequence, 0, charSequence.length, 10)
       }
+      assert(exception.getMessage)(containsString("Error at index"))
     },
   )
 }
