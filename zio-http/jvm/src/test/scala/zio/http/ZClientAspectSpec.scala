34ccd66f2354e03f70da6349ce49e831e9f03255
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-09-19 15:09:30 +0530

    fix 1

--- zio-http/jvm/src/test/scala/zio/http/ZClientAspectSpec.scala
+++ zio-http/jvm/src/test/scala/zio/http/ZClientAspectSpec.scala
@@ -95,9 +95,11 @@ object ZClientAspectSpec extends ZIOHttpSpec {
               URL(Path.empty, Location.Absolute(Scheme.HTTP, "localhost", Some(port))),
             )
             .batched @@ ZClientAspect.followRedirects(2)((resp, message) => ZIO.logInfo(message).as(resp))
-          response <-
-            client.batched(Request.get(URL.decode(s"http://localhost:$port/redirect").toOption.get))
-
+          response <- ZIO.scoped {
+            ZIO.serviceWithZIO[Client](
+              _.batched(Request.get(URL.decode(s"http://localhost:$port/redirect").toOption.get)),
+            )
+          }
         } yield assertTrue(
           extractStatus(response) == Status.Ok,
         ),
@@ -105,7 +107,6 @@ object ZClientAspectSpec extends ZIOHttpSpec {
     ).provide(
       ZLayer.succeed(Server.Config.default.onAnyOpenPort),
       Server.customized,
-      Scope.default,
       ZLayer.succeed(NettyConfig.defaultWithFastShutdown),
       Client.default,
     ) @@ withLiveClock
