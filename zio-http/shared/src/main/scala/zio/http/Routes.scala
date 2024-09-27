9850f6566ecb393536fbaec38dc9c63a4d2768b9
Vamshi Maskuri <117595548+varshith257@users.noreply.github.com>
2024-09-27 15:32:11 +0530

    fmt

--- zio-http/shared/src/main/scala/zio/http/Routes.scala
+++ zio-http/shared/src/main/scala/zio/http/Routes.scala
@@ -254,11 +254,9 @@ final case class Routes[-Env, +Err](routes: Chunk[zio.http.Route[Env, Err]]) { s
           case 0 =>
             if (!Method.knownMethods.contains(req.method)) {
               Handler.status(Status.NotImplemented)
-            }
-            if (!allowedMethods.contains(req.method) && allowedMethods.nonEmpty) {
+            } else if (!allowedMethods.contains(req.method) && allowedMethods.nonEmpty) {
               Handler.status(Status.MethodNotAllowed)
-            }
-            Handler.notFound
+            } else { Handler.notFound }
           case 1 => chunk(0)
           case n => // TODO: Support precomputed fallback among all chunk elements
             var acc = chunk(0)
